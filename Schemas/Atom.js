7a68c240140ac8a4c67f9be3014b71dd8342d236
kdaviesnz <kdavies@gmail.com>
2020-04-05 11:14:27 +1200

    Update Atom.js

--- Schemas/Atom.js
+++ Schemas/Atom.js
@@ -1,3 +1,6 @@
 // ATOM MODEL
-// proton count, velectron1, velectron2, velectron3
-// electrons are strings, v=valence
+// atomic symbol, proton count, velectron1, velectron2, velectron3
+// electrons are unique strings, v=valence
+[
+    "H", 1, "ve1"
+]
