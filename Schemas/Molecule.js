d750a1c104423f5b0f6fddc83dbbc25c775dc536
kdaviesnz <kdavies@gmail.com>
2020-04-05 11:28:38 +1200

    Create Molecule.js

---
+++ Schemas/Molecule.js
@@ -0,0 +1,8 @@
+// MOLECULE MODEL
+// pKa, atom, atom, atom ...
+[
+   7,
+   ["H", 1, 2, "ve1", "ve3"],
+   ["H", 1, 8, "ve2", "ve4"],   
+   ["O", 6, 8, "ve3", "ve4", "ve5", "ve6", "ve7", "ve8", "ve1", "ve2" ], 
+]
