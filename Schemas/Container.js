c14e02b91b5ce917acb6df5c1d08ac3c356c0fb0
kdaviesnz <kdavies@gmail.com>
2020-04-05 10:17:10 +1200

    Update Container.js

---
+++ Schemas/Container.js
@@ -0,0 +1,15 @@
+//  CONTAINER MODEL
+// is vacuum, molecule, molecule ...
+[
+    true,
+    molecule1,
+    molecule2,
+    molecule3
+]
+
+//  CONTAINER CONTROLLER
+{
+    container,
+    add(molecule, units),
+    remove()  
+}
