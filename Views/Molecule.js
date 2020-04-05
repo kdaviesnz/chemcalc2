810f7768d3dda9fedc74e84c05e104005927bc18
kdaviesnz <kdavies@gmail.com>
2020-04-05 19:42:45 +1200

    Create Molecule.js

---
+++ Views/Molecule.js
@@ -0,0 +1,9 @@
+const VMolecule = (mmolecule) => {
+   return {
+       // mmolecule: [pKa, atom, atom, atom ...]
+       // atom: [atomic symbol, proton count, max valence count, velectron1, velectron2,...]
+       canonicalSMILES : () = {
+          
+       }              
+   }
+}
