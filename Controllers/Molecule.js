ad2010463a6d6c224a4e888c2731543c8f42cb98
kdaviesnz <kdavies@gmail.com>
2020-04-05 14:44:40 +1200

    Create  Molecule.js

---
+++ Controllers/Molecule.js
@@ -0,0 +1,17 @@
+
+const CMolecule = (mmolecule) => {
+   return {
+       indexOf : (atom) = {
+          // return mmolecule.indexOf(atom)
+       },
+       push : (atom) = { 
+          // mmolecule.push(atom)
+          return mmolecule
+       },
+       delete : (atom) = {
+          // mmolecule.delete(atom)
+          return mmolecule
+       }
+   }
+}
+
