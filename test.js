d86e580cacdea8e83e8deb96311570ee1a40337b
Kevin Davies <kdavies@gmail.com>
2020-04-06 09:09:03 +1200

    .gitignore

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-const Container = new CContainer([])
+const should = require('should')
+
+const ccontainer = new CContainer([])
 
 // HCl + H2O <-> Cl- + H3O+
-CContainer.add("HCl")
-CContainer.add("water")
+ccontainer.add("HCl")
+ccontainer.container.should.be.greaterThan(2)
+ccontainer.add("water")
