e9b323d69b69509572fa2e6df235efaa41c9c0d2
kdaviesnz <kdavies@gmail.com>
2020-04-05 18:58:25 +1200

    Update Molecule.js

--- Controllers/Molecule.js
+++ Controllers/Molecule.js
@@ -26,14 +26,43 @@ const CMolecule = (mmolecule) => {
           )
           if (atom_to_bond_to_index !== false) {
              atom.push(mmolecule[atom_to_bond_to_index][mmolecule[atom_to_bond_to_index].length - 1])
-             mmolecule[atom_to_bond_to_index].push(atom[atom.length - 1])
+             mmolecule[atom_to_bond_to_index].push(atom[atom.length - 2])
              mmolecule.push(atom)
           }
-          // mmolecule.push(atom)
+          // mmolecule.push(atom)            
           return mmolecule
        },
        delete : (atom) = {
           // mmolecule.delete(atom)
+          if (typeof atom_or_atomic_symbol === "string") {
+             // find index of atom in molecule with matching atomic symbol
+             const atom_index = mmolecule.reduce((carry, current, index)=>{
+                return typeof current === "array" && current[0] === atom_or_atomic_symbol?index:carry
+             }, false)
+ 
+          } else {
+             const atom_index =  mmolecule.search(atom_or_atomic_symbol)
+          }
+
+            if (atom_index === false || mmolecule[atom_index][3] - mmolecule[atom_index].length - 3 === 0 ) {
+                return mmolecule
+             }
+             // Remove bond
+             const electron = mmolecule[atom_index].pop()
+             const bonded_atom_index = mmolecule.reduce((carry, current_atom, index)=>{
+                return typeof current_atom === "array" && current_atom.indexof(electron) !== false
+             }, false)
+             if (bonded_atom_index === false) {
+                // throw an error todo
+                
+             }
+             // remove shared electron
+             delete(mmolecule[bonded_atom_index][current_atom.indexof(electron)]
+             if ( mmolecule[atom_index][3] - mmolecule[atom_index].length - 3 === 0 ) {
+                delete(mmolecule[atom_index] ) 
+             }
+             
+
           return mmolecule
        },
        itemAt : (index) = {
