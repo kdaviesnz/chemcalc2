5cfe674419e7de574e375410647ca9ba8c5f02a4
kdaviesnz <kdavies@gmail.com>
2020-04-05 12:26:03 +1200

    Update Container.js

--- Controllers/Container.js
+++ Controllers/Container.js
@@ -8,6 +8,11 @@ class Container {
     add(molecule_array_or_string, units) {
         const molecule =  (typeof molecule_array_or_string === "array"? molecule_array_or_string:todo) 
         container.push(molecule)
+        // First element is pKa value
+        if (container.length > 2) {
+            const substrate = container[1]
+            const reagent = container[2]
+        }
     },
     
     remove() {
