8b2e932aebb52ea1967b18aee3a6444236259932
kdaviesnz <kdavies@gmail.com>
2020-04-05 17:00:38 +1200

    Update Molecule.js

--- Controllers/Molecule.js
+++ Controllers/Molecule.js
@@ -1,12 +1,18 @@
+// 
 
 const CMolecule = (mmolecule) => {
    return {
+       // mmolecule: [pKa, atom, atom, atom ...]
        // atom: [atomic symbol, proton count, max valence count, velectron1, velectron2,...]
-       indexOf : (atom) = {
-          if (typeof atom === "string") {
-             // find atom in molecule with matching atomic symbol
+       indexOf : (atom_or_atomic_symbol) = {
+          if (typeof atom_or_atomic_symbol === "string") {
+             // find index of atom in molecule with matching atomic symbol
+             return mmolecule.reduce((carry, current, index)=>{
+                return typeof current === "array" && current[0] === atom_or_atomic_symbol?index:carry
+             }, false)
+          } else {
+             return mmolecule.search(atom_or_atomic_symbol)
           }
-          // return mmolecule.indexOf(atom)
        },
        push : (atom) = { 
           // mmolecule.push(atom)
