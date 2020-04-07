a87b70bc2ea972dc28a06bc429d4a37effbc67e6
Kevin Davies <kdavies@gmail.com>
2020-04-08 11:18:12 +1200

    Periodic table

---
+++ Models/PeriodicTable.js
@@ -0,0 +1,19 @@
+const PeriodicTable = {
+// https://www.wuwm.com/post/periodic-table-elements-turns-150#stream/0
+// https://www.wikihow.com/Find-Valence-Electrons
+    "H": {
+        "group":1,
+        "column":"1A",
+        "atomic_number":1,
+        "name":"hydrogen",
+        "atomic_weight":1.008,
+        "electrons_per_shell": 1,
+        "state_of_matter":"gas",
+        "subcategory":"reactive nonmetal"
+    }
+
+
+
+}
+
+module.exports = PeriodicTable
