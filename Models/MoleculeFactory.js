b9e963e966def39fcdd8084a30983132cb644443
kdaviesnz <kdavies@gmail.com>
2020-04-08 18:45:09 +1200

    Update MoleculeFactory.js

--- Models/MoleculeFactory.js
+++ Models/MoleculeFactory.js
@@ -6,15 +6,20 @@ const MoleculeFactory = (canonicalSmiles) => {
     const Canonical_SMILESParser = require("CanonicalSMILESParser")
     SMILESparser = Canonical_SMILESParser(canonicalSmiles)
 
-    const _atoms = () => {
+    const atoms = () => {
         const AtomsFactory = require("AtomsFactory")
         return undefined === atoms || null === atoms?AtomsFactory(canonicalSmiles):atoms
     }
 
+    const pKa = () => {
+        
+    }
+        
     // // MOLECULE MODEL
     // // pKa, atom, atom, atom ...
     return [
-
+        pKa(),
+        ...atoms
     ]
 
 }
