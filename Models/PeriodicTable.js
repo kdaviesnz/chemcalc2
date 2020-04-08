0395c008ec799ec4eb5d3233ad1d7c9593adf3ec
Kevin Davies <kevin@verum.nz>
2020-04-08 13:24:22 +1200

    ..

--- Models/PeriodicTable.js
+++ Models/PeriodicTable.js
@@ -7,10 +7,122 @@ const PeriodicTable = {
         "atomic_number":1,
         "name":"hydrogen",
         "atomic_weight":1.008,
-        "electrons_per_shell": 1,
+        "electrons_per_shell": "1",
         "state_of_matter":"gas",
         "subcategory":"reactive nonmetal"
+    },
+
+    "C": {
+        "group":14,
+        "column":"IVA",
+        "atomic_number":6,
+        "name":"carbon",
+        "atomic_weight":12.001,
+        "electrons_per_shell": "2-4",
+        "state_of_matter":"solid",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "O": {
+        "group":16,
+        "column":"VIA",
+        "atomic_number":8,
+        "name":"oxygen",
+        "atomic_weight":15.999,
+        "electrons_per_shell": "2-6",
+        "state_of_matter":"gas",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "N": {
+        "group":15,
+        "column":"VA",
+        "atomic_number":7,
+        "name":"nitrogen",
+        "atomic_weight":14.007,
+        "electrons_per_shell": "2-5",
+        "state_of_matter":"gas",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "F": {
+        "group":17,
+        "column":"VIIA",
+        "atomic_number":7,
+        "name":"fluorine",
+        "atomic_weight":18.998,
+        "electrons_per_shell": "2-7",
+        "state_of_matter":"gas",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "S": {
+        "group":17,
+        "column":"VIA",
+        "atomic_number":16,
+        "name":"sulfur",
+        "atomic_weight":32.06,
+        "electrons_per_shell": "2-8-6",
+        "state_of_matter":"solid",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "Cl": {
+        "group":17,
+        "column":"VIIA",
+        "atomic_number":17,
+        "name":"chlorine",
+        "atomic_weight":35.45,
+        "electrons_per_shell": "2-8-7",
+        "state_of_matter":"gas",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "Br": {
+        "group":17,
+        "column":"VIIA",
+        "atomic_number":35,
+        "name":"bromine",
+        "atomic_weight":79.904,
+        "electrons_per_shell": "2-8-18-17",
+        "state_of_matter":"liquid",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "I": {
+        "group":17,
+        "column":"VIIA",
+        "atomic_number":53,
+        "name":"iodine",
+        "atomic_weight":126.90,
+        "electrons_per_shell": "2-8-18-18-7",
+        "state_of_matter":"solid",
+        "subcategory":"reactive nonmetal"
+    },
+
+    "Li": {
+        "group":1,
+        "column":"IA",
+        "atomic_number":3,
+        "name":"lithium",
+        "atomic_weight":6.94,
+        "electrons_per_shell": "2-1",
+        "state_of_matter":"solid",
+        "subcategory":"alkali metal"
+    },
+
+    "Na": {
+        "group":1,
+        "column":"IA",
+        "atomic_number":11,
+        "name":"sodium",
+        "atomic_weight":22.98976,
+        "electrons_per_shell": "2-8-1",
+        "state_of_matter":"solid",
+        "subcategory":"alkali metal"
     }
+
+
 
 
 
