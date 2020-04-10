30c2c9e9c9c31bfc22f0d344bb1efcc03bf773aa
kdaviesnz <kdavies@gmail.com>
2020-04-11 11:00:37 +1200

    Update test.js

--- test.js
+++ test.js
@@ -5,11 +5,17 @@ const should = require('should')
 const CContainer = require('./Controllers/Container')
 
 
-const ccontainer = new CContainer([])
+const ccontainer = new CContainer([false])
 
 // HCl + H2O <-> Cl- + H3O+
+//  CONTAINER MODEL
+// is vacuum, molecule, molecule ...
 ccontainer.add("HCl")
 ccontainer.container.length.should.be.equal(2)
+ccontainer.container[0].should.be.equal(false)
+ccontainer.container[1].should.be.array()
+
+
 ccontainer.add("water")
 
 // oOf8ZoXI`@.pE/$DLX2H(FKI7PC$I51
