4c2e604397e766b8562371a1fbcc3e6d3d6b3ff8
kdaviesnz <kdavies@gmail.com>
2020-04-11 15:17:39 +1200

    Update Molecule.js

--- Controllers/Molecule.js
+++ Controllers/Molecule.js
@@ -9,9 +9,35 @@ const CMolecule = (mmolecule) => {
           if (atom_or_atomic_symbol === "H" || atom_or_atomic_symbol[0] === "H") {
              // get molecule atoms that have hydrogens, keeping track of hydrogen indexes
              const candidate_atoms = mmolecule.reduce((carry, current, index)=>{
-                
+                if (current[0] !== "H") {
+                   // check current atom for hydrogens
+                   const H_index = mmolecule.reduce((_carry, _current, _index)=>{
+                      if (_current[0] === "H") {
+                         // Look for bond
+                         if (array_first.filter(function(x) {
+	// checking second array contains the element "x"
+	if(array_second.indexOf(x) != -1)
+		return true;
+	else
+		return false;
+}) === true) {
+                            return _index
+                         } else {
+                            return _carry
+                         }
+                      }
+                   }, -1)
+                   return H_index !== -1?[current[0], H_index]:carry
+                }
              }, [])
-             
+             // check for oxygen atom and if found return the index of hydogen atom bonded to the oxygen atom 
+             const o = candidate_atoms.filter((atom_hydroden_index)=>{
+                return atom_hydroden_index[0]==="O"
+             }
+             if (o.length>0) {
+                return o[0][1]
+             }
+             return candidate_atoms[0][1]
           } 
           else {
              if (typeof atom_or_atomic_symbol === "string") {
