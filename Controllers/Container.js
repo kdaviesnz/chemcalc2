37fc9820de4ad5372933d76acb11c96293a9f6e5
kdaviesnz <kdavies@gmail.com>
2020-04-16 18:15:18 +1200

    Update Container.js

--- Controllers/Container.js
+++ Controllers/Container.js
@@ -4,9 +4,10 @@
 class CContainer {
 
     // container is a container model
-    constructor(container, MoleculeFactory) {
+    constructor(container, MoleculeFactory, MoleculeController) {
         this.container = container
         this.MoleculeFactory = MoleculeFactory
+        this.MoleculeController = MoleculeController
     }
    
     add(molecule_array_or_string, units) {
@@ -20,31 +21,31 @@ class CContainer {
         if (this.container.length > 2) {            
             // acid base
             if (this.container[1].indexOf("H") !== false && this.container[2].indexOf("H") === false) {
-                const proton_index = CMolecule(this.container[1]).indexOf("H")
-                CMolecule.remove(CMolecule(this.container[1]).itemAt(proton_index)) // remove proton
+                const proton_index = this.MoleculeController(this.container[1]).indexOf("H")
+                this.MoleculeController.remove(this.MoleculeController(this.container[1]).itemAt(proton_index)) // remove proton
        
-                this.container[2] = CMolecule(this.container[2]).push("H")
+                this.container[2] = this.MoleculeController(this.container[2]).push("H")
                 
             } else if (this.container[1].indexOf("H") === false && this.container[2].indexOf("H") !== false) {
-                const proton_index = CMolecule(this.container[2]).indexOf("H")
-                CMolecule.remove(CMolecule(this.container[2]).itemAt(proton_index)) // remove proton
+                const proton_index = this.MoleculeController(this.container[2]).indexOf("H")
+                this.MoleculeController.remove(this.MoleculeController(this.container[2]).itemAt(proton_index)) // remove proton
                 
-                this.container[1] = CMolecule(this.container[1]).push("H")
+                this.container[1] = this.MoleculeController(this.container[1]).push("H")
                 
             } else if (this.container[1].indexOf("H") !== false && this.container[2].indexOf("H") !== false) {
                  // HCl + H2O <-> Cl- + H3O+
                  // CC(=O)O (C2H4O2, acetic acid) + water (water is base and accepts proton)
                 // First element is pKa value
                 if (this.container[1][0] < this.container[2][0]) {
-                    const proton_index = CMolecule(this.container[1]).indexOf("H")
-                    CMolecule.remove(CMolecule(this.container[1]).itemAt(proton_index)) // remove proton
-                    CMolecule(this.container[2]).push("H")
+                    const proton_index = this.MoleculeController(this.container[1]).indexOf("H")
+                    this.MoleculeController.remove(this.MoleculeController(this.container[1]).itemAt(proton_index)) // remove proton
+                    this.MoleculeController(this.container[2]).push("H")
                     
                 } else {
-                    const proton_index = CMolecule(this.container[2]).indexOf("H")
-                    CMolecule.remove(CMolecule(this.container[2]).itemAt(proton_index)) // remove proton
+                    const proton_index = this.MoleculeController(this.container[2]).indexOf("H")
+                    this.MoleculeController.remove(this.MoleculeController(this.container[2]).itemAt(proton_index)) // remove proton
                     
-                    CMolecule(this.container[1]).push("H")
+                    this.MoleculeController(this.container[1]).push("H")
                     
                 }
             } else {
