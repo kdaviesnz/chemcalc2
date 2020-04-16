c37687eccc371ea53fbec792ab5304945aebfbb2
kdaviesnz <kdavies@gmail.com>
2020-04-16 18:22:51 +1200

    Update Container.js

--- Controllers/Container.js
+++ Controllers/Container.js
@@ -20,19 +20,19 @@ class CContainer {
         // container[2] is substrate
         if (this.container.length > 2) {            
             // acid base
-            if (this.container[1].indexOf("H") !== false && this.container[2].indexOf("H") === false) {
+            if (this.MoleculeController(this.container[1]).indexOf("H") !== false && this.MoleculeController(this.container[2]).indexOf("H") === false) {
                 const proton_index = this.MoleculeController(this.container[1]).indexOf("H")
                 this.MoleculeController.remove(this.MoleculeController(this.container[1]).itemAt(proton_index)) // remove proton
        
                 this.container[2] = this.MoleculeController(this.container[2]).push("H")
                 
-            } else if (this.container[1].indexOf("H") === false && this.container[2].indexOf("H") !== false) {
+            } else if (this.MoleculeController(this.container[1]).indexOf("H") === false && this.MoleculeController(this.container[2]).indexOf("H") !== false) {
                 const proton_index = this.MoleculeController(this.container[2]).indexOf("H")
                 this.MoleculeController.remove(this.MoleculeController(this.container[2]).itemAt(proton_index)) // remove proton
                 
                 this.container[1] = this.MoleculeController(this.container[1]).push("H")
                 
-            } else if (this.container[1].indexOf("H") !== false && this.container[2].indexOf("H") !== false) {
+            } else if (this.container[1].indexOf("H") !== false && this.MoleculeController(this.container[2]).indexOf("H") !== false) {
                  // HCl + H2O <-> Cl- + H3O+
                  // CC(=O)O (C2H4O2, acetic acid) + water (water is base and accepts proton)
                 // First element is pKa value
