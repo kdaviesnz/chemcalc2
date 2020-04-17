6fd39282da59fa372ccf84e4ee62bdb958cfcb22
kdaviesnz <kdavies@gmail.com>
2020-04-17 17:30:51 +1200

    Update Molecule.js

--- Views/Molecule.js
+++ Views/Molecule.js
@@ -1,10 +1,18 @@
 const VMolecule = (mmolecule) => {
    return {
        // mmolecule: [pKa, atom, atom, atom ...]
-       // atom: [atomic symbol, proton count, max valence count, velectron1, velectron2,...]
+       // atom: [atomic symbol, proton count, max valence count, std bond count, velectron1, velectron2,...]
        canonicalSMILES : () = {
           if (mmolecule.length === 2) {
-             return mmolecule[1][0]
+             // Get number of actual bonds atom has
+             const bond_count  = CMolecule(mmolecule).bondCount(mmolecule[1])
+             if (bond_count > mmolecule[1][3]) {
+                return "[" + mmolecule[1][0] + "+]"
+             } elseif (bond_count > mmolecule[1][3]) {
+                return "[" + mmolecule[1][0] + "-]"
+             } else
+                return mmolecule[1][0]
+             }
           }
        }              
    }
