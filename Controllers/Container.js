037813e7a00c7c66c630ba0a5924dff5061aa85c
Kevin Davies <kdavies@gmail.com>
2020-04-19 17:45:28 +1200

    testing

--- Controllers/Container.js
+++ Controllers/Container.js
@@ -38,11 +38,68 @@ class CContainer {
                 // First element is pKa value
                 if (this.container[1][0] < this.container[2][0]) {
                     const proton_index = this.MoleculeController(this.container[1]).indexOf("H")
-                    this.container = this.MoleculeController(this.container[2]).remove(container, 1,this.MoleculeController(this.container[1]).itemAt(proton_index)) // remove proton
+                    this.container = this.MoleculeController(this.container[2]).remove(this.container, 1,this.MoleculeController(this.container[1]).itemAt(proton_index)) // remove proton
                     this.MoleculeController(this.container[2]).push("H")
                     
                 } else {
                     const proton_index = this.MoleculeController(this.container[2]).indexOf("H")
+                    console.log("container")
+                    console.log(this.container)
+                    console.log("proton index:")
+                    console.log(proton_index)
+                    /*
+                    container
+[ false,
+  [ 9999,
+    [ 'H', 1, 1, 1, 'w2uspk96mjnja', 'w2uspk96mjnj3' ],
+    [ 'Cl',
+      17,
+      7,
+      1,
+      'w2uspk96mjnj3',
+      'w2uspk96mjnj4',
+      'w2uspk96mjnj5',
+      'w2uspk96mjnj6',
+      'w2uspk96mjnj7',
+      'w2uspk96mjnj8',
+      'w2uspk96mjnj9',
+      'w2uspk96mjnja' ] ],
+  [ 9999,
+    [ 'H', 1, 1, 1, 'w2uspk96mjnjh', 'w2uspk96mjnjb' ],
+    [ 'H', 1, 1, 1, 'w2uspk96mjnji', 'w2uspk96mjnjc' ],
+    [ 'O',
+      8,
+      6,
+      2,
+      'w2uspk96mjnjb',
+      'w2uspk96mjnjc',
+      'w2uspk96mjnjd',
+      'w2uspk96mjnje',
+      'w2uspk96mjnjf',
+      'w2uspk96mjnjg',
+      'w2uspk96mjnjh',
+      'w2uspk96mjnji' ] ] ]
+proton index:
+2
+[ 'H', 1, 1, 1, 'w2uspk96mjnji', 'w2uspk96mjnjc' ]
+[ 9999,
+  [ 'H', 1, 1, 1, 'w2uspk96mjnja', 'w2uspk96mjnj3' ],
+  [ 'Cl',
+    17,
+    7,
+    1,
+    'w2uspk96mjnj3',
+    'w2uspk96mjnj4',
+    'w2uspk96mjnj5',
+    'w2uspk96mjnj6',
+    'w2uspk96mjnj7',
+    'w2uspk96mjnj8',
+    'w2uspk96mjnj9',
+    'w2uspk96mjnja' ] ]
+atom_index
+-1
+
+                     */
                     this.container = this.MoleculeController(this.container[1]).remove(this.container, 2, this.MoleculeController(this.container[2]).itemAt(proton_index)) // remove proton
                     
                     this.MoleculeController(this.container[1]).push("H")
