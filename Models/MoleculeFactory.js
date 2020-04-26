06807b3d3a7214b2571d723c76eb3f6fdcee8ffe
kdaviesnz <kdavies@gmail.com>
2020-04-26 12:07:42 +1200

    Update MoleculeFactory.js

--- Models/MoleculeFactory.js
+++ Models/MoleculeFactory.js
@@ -16,6 +16,7 @@ const MoleculeFactory = (canonicalSmiles) => {
 
     const pKa = () => {
         // @todo
+        // https://www2.onu.edu/~b-myers/organic/2511_Files/Chapter3-pKa%20table.pdf
         const map = {
             "CC(=O)O":4.76,
             "Cl":-7,
