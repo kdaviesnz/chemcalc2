7cdfe9436d2e16e063b607e6211f1d9435ffb45d
Kevin Davies <kdavies@gmail.com>
2020-05-03 09:42:12 +1200

    functional groups

--- Models/FunctionalGroups.js
+++ Models/FunctionalGroups.js
@@ -6,37 +6,37 @@ const FunctionalGroups = (atoms) => {
     // atoms
     //[[ atomic symbol, proton count, valence count,  number of bonds, velectron1, velectron2, velectron3 ]]
 
-    
-    const __carbonToAtomDoubleBond = (atom, oxygen_atom_index) => {
-          
-                if (atom[0] === "0") {
-                    // Determine if there is a double bond
-                    // Double bond if we have two electrons linking to the same carbon
-                    const oxygen_electrons =
+
+    const __carbonToAtomDoubleBondCallback = (atomic_symbol) => {
+
+        return (atom, atom_index) => {
+            if (atom[0] === atomic_symbol) {
+                // Determine if there is a double bond
+                // Double bond if we have two electrons linking to the same carbon
+                const atom_electrons =
                     atoms.filter(
                         (_atom, carbon_atom_index) => {
                             if (_atom[0] === "C") {
                                 const carbon_electrons = _atom.splice(4)
-                                // Get intersection of carbon electrons and oxygen electrons. If count
+                                // Get intersection of carbon electrons and atom electrons. If count
                                 // is 2 then we have a double bond
-                                if (Set().intersection(carbon_electrons, oxygen_electrons).length===2){
+                                if (Set().intersection(carbon_electrons, arom_electrons).length === 2) {
                                     // We have a ketone
-                                    // return indexed carbon and oxygen
+                                    // return indexed carbon and atom
                                     return {
                                         carbon_atom_index: _atom[0],
-                                        oxygen_atom_index: atom[0]
+                                        atom_index: atom[0]
                                     }
                                 }
                             }
                             return false
                         }
                     )
-                }
-                return false
-          
+            }
+        }
+
     }
-                                      
-                                      
+
     const ketone = () => {
 
         // If no oxygen atom then not ketone
@@ -49,12 +49,11 @@ const FunctionalGroups = (atoms) => {
         }
 
         // If carbon double bond on oxygen then not ketone
-        if (atoms.filter(
-          return __carbonToAtomDoubleBond = (atom, oxygen_atom_index)
-            
-        ).length === 0) {
-            return false
-        }
+        return atoms.map(
+          __carbonToAtomDoubleBondCallback ("O")
+        )
+
+
 
     }
 
@@ -69,6 +68,9 @@ const FunctionalGroups = (atoms) => {
             return false
         }
 
+        /*
+        In organic chemistry, an amide, also known as an organic amide or a carboxamide, is a compound with the general formula RC(=O)NR′R″
+         */
         return ketone()
 
     }
@@ -78,7 +80,7 @@ const FunctionalGroups = (atoms) => {
         /*
         Glycol, any of a class of organic compounds belonging to the alcohol family; in the molecule of a glycol, two hydroxyl (―OH) groups are attached to different carbon atoms. The term is often applied to the simplest member of the class, ethylene glycol.
          */
-         // If there isn't at least 2 OH groups then not glycol
+        // If there isn't at least 2 OH groups then not glycol
         if (atoms.filter(
             (atom) => {
                 return atom[0] === "O"
@@ -239,28 +241,28 @@ const FunctionalGroups = (atoms) => {
         // C=CCC1=CC2=C(C=C1)OCO2
         return canonical_SMILES.indexOf("C=C")===0?[canonical_SMILES]:false
     }
-    
-const __split_alkene = (left, SMILES, right ) => {
-    const alkene_groups = canonical_SMILES.match(/(.*?C[0-9]*?)\=(C[0-9]*.*)/)
+
+    const __split_alkene = (left, SMILES, right ) => {
+        const alkene_groups = canonical_SMILES.match(/(.*?C[0-9]*?)\=(C[0-9]*.*)/)
 // console.log(alkene_molecule.CanonicalSMILES.match(/(.*?C[0-9]*?)\=(C[0-9]*.*)/))
-/*
-[ 'C=CCC1=CC2=C(C=C1)OCO2',
-  'C',
-  'CCC1=CC2=C(C=C1)OCO2',
-  index: 0,
-  input: 'C=CCC1=CC2=C(C=C1)OCO2' ]
- */
-if (alkene_groups === null) {
-    return false
-}
-    return [[left + alkene_groups[1], alkene_groups[2] + right]]
-}
+        /*
+        [ 'C=CCC1=CC2=C(C=C1)OCO2',
+          'C',
+          'CCC1=CC2=C(C=C1)OCO2',
+          index: 0,
+          input: 'C=CCC1=CC2=C(C=C1)OCO2' ]
+         */
+        if (alkene_groups === null) {
+            return false
+        }
+        return [[left + alkene_groups[1], alkene_groups[2] + right]]
+    }
 
 
     const alkene = () => {
         // C=CCC1=CC2=C(C=C1)OCO2
 
-       const alkene_groups_formatted = __split_alkene("", canonical_SMILES, "")
+        const alkene_groups_formatted = __split_alkene("", canonical_SMILES, "")
 
         if (alkene_groups_formatted===false) {
             return false
@@ -269,9 +271,9 @@ if (alkene_groups === null) {
         const alkene_group = __split_alkene(alkene_groups_formatted[0], alkene_groups_formatted[0], "")
 
 
-      alkene_groups_formatted.push(alkene_group)
+        alkene_groups_formatted.push(alkene_group)
         return alkene_groups_formatted
-        
+
         /*
         return alkene_groups_formatted.reduce(
            (total, currentValue, currentIndex, arr)=> {
