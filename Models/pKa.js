3972d1fa83c50cb0be2ee06e60ba5f40acfe1bc9
kdaviesnz <kdavies@gmail.com>
2020-05-03 16:05:11 +1200

    Update pKa.js

--- Models/pKa.js
+++ Models/pKa.js
@@ -1,7 +1,8 @@
 const pKa = (atoms) => {
 // https://sciencing.com/calculate-pka-values-2765.html
   // https://www.masterorganicchemistry.com/2010/06/18/know-your-pkas/
-  return fg.functionalGroups.hydrochloric_acid.length > 0?
+  return fg.functionalGroups.hydrochloric_acid.length > 0? -6
+  :9999
   
   
 }
