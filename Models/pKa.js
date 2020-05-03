eda905339c6133677cd2c06a175c1aa78ce326f2
kdaviesnz <kdavies@gmail.com>
2020-05-03 16:45:24 +1200

    Update pKa.js

--- Models/pKa.js
+++ Models/pKa.js
@@ -1,8 +1,11 @@
+const fg = require("FunctionalGroups")
 const pKa = (atoms) => {
 // https://sciencing.com/calculate-pka-values-2765.html
   // https://www.masterorganicchemistry.com/2010/06/18/know-your-pkas/
   return fg.functionalGroups.hydrochloric_acid.length > 0? -6
-  :9999
+  :fg.functionalGroups.deprotonated_hydrochloric_acid.length > 0? 888
+  :fg.functionalGroups.water.length > 0? 14
+  :fg.functionalGroups.protonated_water.length > 0? -1.74
   
   
 }
