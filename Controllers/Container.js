82cf1f9f8b33d7243a32ab5876cb282bd88f9806
kdaviesnz <kdavies@gmail.com>
2020-05-10 13:47:29 +1200

    Update Container.js

--- Controllers/Container.js
+++ Controllers/Container.js
@@ -39,21 +39,32 @@ class CContainer {
 
             // The functional group of an alkene is the C=C double bond.
             // The C=C double bond is nucleophilic
+            let reaction = false
             if (substrate_families.alkene.length > 0) {
+                
+                // Substrate is alkene
                 // Find the nucleophile on the C=C bond
                 const nucleophile_atom_index = substrate_families.alkene[0][0]
                 substrate[nucleophile_atom_index][0].should.be.equal("C")
                 AtomController(substrate[nucleophile_atom_index], nucleophile_atom_index, substrate.slice(1)).bondCount.should.be.equal(2)
-                bronstedLowry.react(substrate, nucleophile_atom_index, reagent, null)
+                reaction =bronstedLowry.react(substrate, nucleophile_atom_index, reagent, null)) {
+                    
+                
 
             } elseif (Families(reagent.slice(1)).families.alkene.length > 0) {
                 
+                // Reagent is alkene
                 // Find the nucleophile on the C=C bond
                 const nucleophile_atom_index = reagent_families.alkene[0][0]
                 reagent[nucleophile_atom_index][0].should.be.equal("C")
                 AtomController(reagent[nucleophile_atom_index], reagent_atom_index, reagent.slice(1)).bondCount.should.be.equal(2)
-               
+                reaction = bronstedLowry.react(reagent, nucleophile_atom_index, substrate, null)) {
+                                   
                 
+            }
+                
+            if (!reaction) {
+                // No alkene
             }
 
             BronstedLowryAcidBaseReactions(this.container, this.MoleculeController, this.test_number)
