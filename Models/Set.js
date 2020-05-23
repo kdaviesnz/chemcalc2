ed3bcbd57d993768247b1e7df5b488581600f777
kdaviesnz <kdavies@gmail.com>
2020-05-23 17:38:43 +1200

    Update Set.js

--- Models/Set.js
+++ Models/Set.js
@@ -1,8 +1,12 @@
 const Set = () => {
+    
+    
+    
     return {
         intersection: (array1, array2) => {
             return array1.filter((value) => array2.includes(value))
         }
+        
     }
 }
 
