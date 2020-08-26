c546899d98d9aeeca727e207d44f77349caf6332
Kevin Davies <kevin@verum.nz>
2020-08-27 11:45:08 +1200

    temp.js

---
+++ temp.js
@@ -0,0 +1,58 @@
+const f = {
+    "fact":"MDMA produces increased empathy for oneself and others",
+    "keywords":["empathy"],
+    "references": [
+        {
+            "link": {
+                "authors": ["Sessa, B."],
+                "title": "Why MDMA therapy for alcohol use disorder? And why now?, Neuropharmacology",
+                "year": 2017,
+                "uri": "https://doi.org/10.1016/j.neuropharm.2017.11.004"
+            },
+            "page": 3,
+            "excerpt": "MDMA produces slight alterations in perception that facilitate imagination and memory (Harris et al., 2002), increased positive mood, increased feelings of closeness, greater compassion and increased empathy for oneself and others (Hysek et al., 2013).",
+            "keywords": ["empathy", "positive effects", "cognitive effects"],
+            "referenced_links": [
+                {
+                    "authors": ["Harris, D.S.", "Baggott, M.", "Mendelson, J.H.", "Mendelson, J.E.", "Jones, R.T."],
+                    "title": "Subjective and hormonal effects of 3,4- methylenedioxymethamphetamine (MDMA) in humans. Psychopharmacol. Berl. 162 (4), 396e405.",
+                    "year": 2002,
+                    "uri": "https://pubmed.ncbi.nlm.nih.gov/12172693/"
+                },
+                {
+                    "authors": ["Hysek, C.M."],
+                    "title": "MDMA enhances emotional empathy and prosocial behavior. Soc. Cogn. Affect Neurosci. 9 (11), 1645e1652. Nov",
+                    "year": 2013,
+                    "uri": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4221206/"
+                }
+            ]
+        }
+    ]
+}
+
+const blank = {
+    "fact":"",
+    "keywords":[],
+    "references": [
+        {
+            "link": {
+                "authors": [],
+                "title": "",
+                "year": 2020,
+                "uri": ""
+            },
+            "page": 1,
+            "excerpt": "",
+            "keywords": [],
+            "referenced_links": [
+                {
+                    "authors": [],
+                    "title": "",
+                    "year": 2020,
+                    "uri": ""
+                }
+            ]
+        }
+    ]
+}
+
