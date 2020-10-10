4718d1a6755638cc22c97d5fe9b0939a51597127
Kevin Davies <kdavies@gmail.com>
2020-10-10 14:44:07 +1300

    ...

--- Commands/AddProton.js
+++ Commands/AddProton.js
@@ -1,13 +1,9 @@
 const MoleculeController = require('../controllers/MoleculeController')
-const FindDoubleBondPair = require('./FindDoubleBondPair')
+//const FindDoubleBondPair = require('./FindDoubleBondPair')
 
-const AddProton = (target_molecule, proton, target_molecule_atom_index = null) => {
-    if (null == target_molecule_atom_index) {
-        const molecule_protonation_target_atom = FindProtonationTargetAtom(target_molecule, FindDoubleBondPair(target_molecule))
-        target_molecule_atom_index = molecule_protonation_target_atom.index
-    }
-    const target_molecule_proton_removed_controller = MoleculeController(target_molecule)
-    return target_molecule_proton_removed_controller.addProton(proton,target_molecule.atoms[target_molecule_atom_index]).products
+const AddProton = (mmolecule, reagent) => {
+    const target_molecule_proton_removed_controller = MoleculeController(mmolecule)
+    //return target_molecule_proton_removed_controller.addProton(proton,target_molecule.atoms[target_molecule_atom_index]).products
 }
 
 module.exports = AddProton
--- Commands/RemoveProton.js
+++ Commands/RemoveProton.js
@@ -1,8 +1,8 @@
 const MoleculeController = require('../controllers/MoleculeController')
 
-const RemoveProton = (target_molecule, proton) => {
-    const target_molecule_controller = MoleculeController(target_molecule)
-    return target_molecule_controller.removeProton(proton).products
+const RemoveProton = (mmolecule, reagent) => {
+    const target_molecule_controller = MoleculeController(mmolecule)
+   // return target_molecule_controller.removeProton(proton).products
 }
 
 module.exports = RemoveProton
