0ec3e9013d781358b34c2b9d05513c35349b13ad
kdaviesnz <kdavies@gmail.com>
2020-10-10 18:23:59 +1300

    Update BreakBond.js

--- Commands/BreakBond.js
+++ Commands/BreakBond.js
@@ -1,1 +1,15 @@
-mmolecule_atom_index, mmolecule_atom_index
+const Reaction = require("../Components/State/Reaction")
+
+const BreakBond = (mmolecule, reagent, mmolecule_atom_index, reagent_atom_index) => {
+
+    const reaction = new Reaction(mmolecule, reagent)
+
+    reaction.breakBond(mmolecule_atom_index, reagent_atom_index)
+
+    return [
+        reaction.mmolecule,
+        reaction.reagent
+    ]
+}
+
+module.exports = BreakBond
