f7d26fb482ceb57cc9b24c70a674c1a6c003d59f
Kevin Davies <kevin@verum.nz>
2020-10-22 10:16:19 +1300

    ...

--- testcommands.js
+++ testcommands.js
@@ -7,24 +7,21 @@ const CAtom = require('./Controllers/Atom')
 
 const CommandTest = require('./Components/Stateless/CommandTest')
 
+// PROTONATE
 // See Organic Chemistry 8th Edition p245
 const alkene = MoleculeFactory("CC=C")
 const alkene_ai = require("./Components/Stateless/MoleculeAI")([alkene,1])
-/*
-[ [ 'C', 3, [ '5  C' ] ],
-  [ 'C', 5, [ '3  C', '8  C' ] ],
-  [ 'C', 8, [ '5  C' ] ] ]
- */
 const nucleophile_index =alkene_ai.findNucleophileIndex()
 alkene_ai.findNucleophileIndex().should.be.equal(8)
 CAtom(alkene[1][nucleophile_index], nucleophile_index, [alkene,1]).freeElectrons().length.should.be.equal(0)
 const double_bonds = CAtom(alkene[1][nucleophile_index], nucleophile_index, [alkene,1]).indexedDoubleBonds("")
 double_bonds.length.should.be.equal(1)
 const protonated_products = CommandTest("PROTONATE", [alkene,1], [MoleculeFactory("OS(=O)(=O)O"),1])
-//console.log(VMolecule(protonated_products[0]).compressed())
-
 VMolecule(protonated_products[0]).canonicalSMILES().should.be.equal('C[CH1+]C')
 process.exit()
+
+
+// DEPROTONATE
 const isopropyl_cation = protonated_products[0]
 
 console.log(VMolecule(protonated_products[1]).canonicalSMILES())
