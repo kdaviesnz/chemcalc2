4df6d5d4e2550b5744c2395c7f572730f55369ae
Kevin Davies <kevin@verum.nz>
2020-10-21 16:40:54 +1300

    ...

--- testcommands.js
+++ testcommands.js
@@ -7,17 +7,13 @@ const MoleculeFactory = require('./Models/MoleculeFactory')
 
 const CommandTest = require('./Components/Stateless/CommandTest')
 
-// [PROTONATE] COC(C)(C)(=C) + OS(=O)(=O)(O) = COC(C)(C)([C+])
+// See Organic Chemistry 8th Edition p245
+const alkene = MoleculeFactory("CC=C")
+const protonated_products = CommandTest("PROTONATE", [alkene,1], [MoleculeFactory("OS(=O)(=O)O"),1])
+console.log(VMolecule(protonated_products[0]).canonicalSMILES())
+console.log(VMolecule(protonated_products[1]).canonicalSMILES())
+/*
+CC=[C+]
+OS(=O)(=O)O
 
-// Incorrect
-// [HYDRATE] COC(C)(C)([CH2+]) + O = COC(C)(C)(C[OH2+])
-// Should be
-// [HYDRATE] COC(C)(C)([CH3+]) + O = COC(C)(C)(C[OH2+])
-// DEHYDRATE command is wrong
-
-
-// Correct
-// [REMOVE proton from water] COC(C)(C)(C[OH2+]) + [O+] = COC(C)(C)(CO)
-const protonate_products = CommandTest("PROTONATE", [MoleculeFactory("COC(C)(C)(=C)"),1], [MoleculeFactory("OS(=O)(=O)(O)"),1])
-console.log(VMolecule(products[0]).canonicalSMILES())
-console.log(VMolecule(products[1]).canonicalSMILES())
+ */
