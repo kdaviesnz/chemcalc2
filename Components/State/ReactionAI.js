c179b6248879287ec172657b4b982c204d75737b
Kevin Davies <kdavies@gmail.com>
2020-12-15 18:23:50 +1300

    ...

---
+++ Components/State/ReactionAI.js
@@ -0,0 +1,22 @@
+// Protonation
+
+// Organic chemistry 8th edition p245
+// https://www.chemistrysteps.com/oxymercuration-demercuration/
+// Addition of water to alkene (C=C)
+// Rules
+// if C=C bond then protonate (reagent = acid)
+// Reversal
+// if C+ (carbocation) convert [C+]C bond to C=C, removing proton from C.
+
+// Epoxide acidic ring opening
+// https://chem.libretexts.org/Bookshelves/Organic_Chemistry/Map%3A_Organic_Chemistry_(McMurry)/Chapter_18%3A_Ethers_and_Epoxides%3B_Thiols_and_Sulfides/18.06_Reactions_of_Epoxides%3A_Ring-opening
+// if O atom on ring then protonate O (reagant = acid)
+// Reversal
+// if [O+] atom on ring then deprotonate [O+] (reagent = conjugate base)
+
+
+// https://en.wikipedia.org/wiki/Pinacol_rearrangement
+// if OH (hyrodxyl oxygen) protonate OH (reagent = acid)
+// Reversal
+// if water group ([O+] then deprotonate [O+] (reagent = conjugate base)
+
