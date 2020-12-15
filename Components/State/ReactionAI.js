8feb0befa73aa640f0148d230469c500dcf609b5
Kevin Davies <kdavies@gmail.com>
2020-12-15 19:06:38 +1300

    ...

--- Components/State/ReactionAI.js
+++ Components/State/ReactionAI.js
@@ -10,13 +10,53 @@
 
 // Epoxide acidic ring opening
 // https://chem.libretexts.org/Bookshelves/Organic_Chemistry/Map%3A_Organic_Chemistry_(McMurry)/Chapter_18%3A_Ethers_and_Epoxides%3B_Thiols_and_Sulfides/18.06_Reactions_of_Epoxides%3A_Ring-opening
+// https://chem.libretexts.org/Courses/Sacramento_City_College/SCC%3A_Chem_420_-_Organic_Chemistry_I/Text/09%3A_Reactions_of_Alkenes/9.13%3A_Dihydroxylation_of_Alkenes
 // if O atom on ring then protonate O (reagant = acid)
 // Reversal
 // if [O+] atom on ring then deprotonate [O+] (reagent = conjugate base)
 
 
 // https://en.wikipedia.org/wiki/Pinacol_rearrangement
+// https://en.wikipedia.org/wiki/Leuckart_reaction
 // if OH (hyrodxyl oxygen) protonate OH (reagent = acid)
 // Reversal
 // if water group ([O+] then deprotonate [O+] (reagent = conjugate base)
+
+//https://chem.libretexts.org/Bookshelves/Organic_Chemistry/Map%3A_Organic_Chemistry_(Smith)/Chapter_22%3A_Carboxylic_Acids_and_Their_Derivatives—_Nucleophilic_Acyl_Substitution/22.11%3A_Reactions_of_Esters"
+// if oxygen on double bond then protonate oxygen (reagent = acid)
+// Reversal
+// if [O+]=C then deprotonate [O+] (reagent = conjugate base)
+
+// https://chem.libretexts.org/Bookshelves/Organic_Chemistry/Map%3A_Organic_Chemistry_(McMurry)/18%3A_Ethers_and_Epoxides_Thiols_and_Sulfides/18.06%3A_Reactions_of_Epoxides_-_Ring-opening
+// https://en.wikipedia.org/wiki/Leuckart_reaction
+// if [O-] atom then protonate [O-] atom (reagent = acid)
+// Reversal
+// if OH (hyrodxyl oxygen) deprotonate OH (reagent = conjugate base)
+
+// https://en.wikipedia.org/wiki/Ritter_reaction
+// if [N+] then protonate [N+] (reagent = acid)
+// Reversal
+// if N with H then deprotonate N (reagent = conjugate base)
+
+const Reaction = require("../Components/State/Reaction")
+
+class ReactionAI {
+
+    constructor() {
+
+    }
+
+    synthesise(target) {
+        const moleculeAI = require("../Stateless/MoleculeAI")(target)
+        this.deprotonate(target, moleculeAI, this.synthesise)
+    }
+
+    deprotonate(target, moleculeAI, callback) {
+
+        const conjugate_base_hydroxide = MoleculeFactory("[O-]")
+        const reaction = new Reaction(target, [conjugate_base_hydroxide, 1], {})
+
+
+    }
+}
 
