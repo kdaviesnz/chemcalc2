1fcc12e967d5cccb7cdbb51c639068122382de1c
Kevin Davies <kdavies@gmail.com>
2021-03-23 17:34:49 +1300

    ...

--- Components/Stateless/Views/Reactions.js
+++ Components/Stateless/Views/Reactions.js
@@ -10,71 +10,46 @@ const pubchem = require("pubchem-access").domain("compound");
 
 const VReaction = (db, reactions, container_end_product, rule) => {
 
-    const pkl = PubChemLookup((err)=>{
-        console.log(err)
-        process.exit()
-    })
 
     const onErrorLookingUpMoleculeInDB = (Err) => {
         console.log(Err)
-        client.close();
         process.exit()
     }
 
-    const onMoleculeNotFound =  (onMoleculeAddedToDBCallback) => {
-        return (search) => {
-            console.log("Molecule not found " + search)
-            pkl.searchBySMILES(search.replace(/\(\)/g, ""), db, (molecule_from_pubchem) => {
-                if (molecule_from_pubchem !== null) {
-                    console.log("Molecule found in pubchem")
-                    molecule_from_pubchem['json'] = MoleculeFactory(search)
-                    molecule_from_pubchem['search'] = search
-                    db.collection("molecules").insertOne(molecule_from_pubchem, (err, result) => {
-                        console.log("Inserted molecule")
-                        console.log(molecule_from_pubchem)
-                        //console.log(aaalll)
-                        //process.exit()
-                        if (err) {
-                            console.log(err)
-                            process.exit()
-                        } else {
-                            onMoleculeAddedToDBCallback(search)
-                        }
-                    })
-
-                }
-            })
-        }
-    }
-
-    const addFinishReagent = function(reaction, substrate, product, reagent) {
+    const addFinishReagent = function(reactions, index, reaction, substrate, product, reagent) {
         if (typeof reaction.finish_reagent[0] === "string") {
             const finish_reagent = reaction.finish_reagent[0]
-            renderLine(reaction, substrate, product, reagent, finish_reagent)
+            renderLine(reactions, index, reaction, substrate, product, reagent, finish_reagent)
         } else {
             MoleculeLookup(db, VMolecule(reaction.finish_reagent).canonicalSMILES(), "SMILES", true).then(
                 (finish_reagent_json_obj) => {
-                    console.log("Finish Reagent:")
-                    console.log(finish_reagent_json_obj)
+                   // console.log("Finish Reagent:")
+                   // console.log(finish_reagent_json_obj)
                     const finish_reagent = undefined === finish_reagent_json_obj.IUPACName?finish_reagent_json_obj.search:finish_reagent_json_obj.IUPACName
-                    renderLine(reaction, substrate, product, reagent, finish_reagent)
-                }
+                    renderLine(reactions, index, reaction, substrate, product, reagent, finish_reagent)
+                },
+                onErrorLookingUpMoleculeInDB
             )
         }
     }
 
-    const renderLine = function(reaction, substrate, product, reagent, finish_reagent) {
+    const renderLine = function(reactions, index, reaction, substrate, product, reagent, finish_reagent) {
+
         console.log("[" + reaction.command.bold.red + "] "
             + substrate.green
             +  ' + ' + reagent.yellow
             + " -> " + product.bold +  ' + ' + finish_reagent)
-        console.log(linnki)
-        // renderReactionsRecursive(reactions, index + 1)
+        //console.log("reactions:")
+        //console.log(reactions)
+        //console.log(aabb)
+        renderReactionsRecursive(reactions, index + 1)
         // callback(Err, reaction)
     }
 
     const renderReactionsRecursive = function(reactions, index) {
+
         const reaction = reactions[index]
+
 
         if (reaction !== undefined) {
 
@@ -86,65 +61,54 @@ const VReaction = (db, reactions, container_end_product, rule) => {
                 reaction.reagent = null
             }
 
+
             MoleculeLookup(db, VMolecule(reaction.substrate).canonicalSMILES(), "SMILES", true).then(
                 // "resolves" callback
                 (substract_json_obj) => {
 
-                    console.log("Substrate:")
-                    console.log(substract_json_obj)
-                    const substrate = undefined === substract_json_obj.IUPACName?substract_json_obj.search:substract_json_obj.IUPACName
+                    const substrate = (undefined === substract_json_obj.IUPACName?substract_json_obj.search:substract_json_obj.IUPACName)
 
                     MoleculeLookup(db,VMolecule(reaction.product).canonicalSMILES(), "SMILES", true).then(
 
                         (product_json_obj) => {
 
-                            console.log("Product:")
-                            console.log(product_json_obj)
+                           // console.log("Product:")
+                           // console.log(product_json_obj)
                             const product = undefined === product_json_obj.IUPACName?product_json_obj.search:product_json_obj.IUPACName
 
                             if (reaction.reagent === undefined || reaction.reagent === null) {
                                 const reagent = "no reagent"
-                                addFinishReagent(reaction, substrate, product, reagent)
+                                addFinishReagent(reactions, index, reaction, substrate, product, reagent)
                             } else if(typeof reaction.reagent[0] === "string") {
                                 const reagent = reaction.reagent[0]
-                                addFinishReagent(reaction, substrate, product, reagent)
+                                addFinishReagent(reactions, index, reaction, substrate, product, reagent)
                             } else {
                                 MoleculeLookup(db, VMolecule(reaction.reagent).canonicalSMILES(), "SMILES", true).then(
                                     (reagent_json_obj) => {
-                                        console.log("Reagent:")
-                                        console.log(reagent_json_obj)
+                                      //  console.log("Reagent:")
+                                      //  console.log(reagent_json_obj)
                                         const reagent = undefined === reagent_json_obj.IUPACName?reagent_json_obj.search:reagent_json_obj.IUPACName
-                                        addFinishReagent(reaction, substrate, product, reagent)
+                                        addFinishReagent(reactions, index, reaction, substrate, product, reagent)
                                     },
-                                    onMoleculeNotFound((search) => {
-                                        console.log("Molecule " + search + " added to database")
-                                        process.exit()
-                                    }),
                                     // "rejects" callback
                                     onErrorLookingUpMoleculeInDB
                                 )
                             }
 
                         },
-                        onMoleculeNotFound((search) => {
-                            console.log("Molecule " + search + " added to database")
-                            process.exit()
-                        }),
-                        // "rejects" callback
                         onErrorLookingUpMoleculeInDB
+
                     )
 
                 },
-                // Nothing found callback
-                onMoleculeNotFound((search) => {
-                    console.log("Molecule " + search + " added to database")
-                    process.exit()
-                }),
                 // "rejects" callback
                 onErrorLookingUpMoleculeInDB
             )
 
 
+        } else {
+
+            process.exit()
         }
     }
 
--- Controllers/MoleculeLookup.js
+++ Controllers/MoleculeLookup.js
@@ -1,50 +1,88 @@
 // MoleculeLookup
 //const PubChemLookup    = require('../lib/PubChemLookup')
 const MoleculeFactory = require('../Models/MoleculeFactory')
+const PubChemLookup = require('../Controllers/PubChemLookup')
+const pubchem = require("pubchem-access").domain("compound");
+const pkl = PubChemLookup((err)=>{
+    console.log(err)
+    process.exit()
+})
+
 
 const MoleculeLookup = (db, search, search_type, add_hydrogens, debug_statement, Err) =>
     new Promise(
-        (resolve, notFound, reject) => {
+        (resolve, reject) => {
             const search_no_quotes = search.replace(/['"]+/g, '').trim()
-            db.collection('molecules').findOne(
-                {$or:[{"CanonicalSMILES":search}, {"search":search_no_quotes}, {"IUPACName":search_no_quotes}]},
-                function (Err, molecule) {
+                db.collection('molecules').findOne(
+                    {$or: [{"CanonicalSMILES": search}, {"search": search_no_quotes}, {"IUPACName": search_no_quotes}]},
+                    function (Err, molecule) {
 
-                    // An error occurred
-                    if (Err) {
-                        console.log(Err)
-                        process.exit()
-                        // reject(Err)
-                    }
-
-                    if (null === molecule) {
-                        notFound(search)
-                    } else {
-                        // molecule found in db
-                        // add json if required
-                        if (undefined === molecule.json) {
-                            const molecule_json = MoleculeFactory(search)
-                            db.collection('molecules').updateOne(
-                                {CID: molecule.CID},
-                                {$set:{json:molecule_json}},
-                                {upsert:true},
-                                (err, count, status) => {
-                                    if (err) {
-                                        console.log("Error adding json")
-                                        process.exit()
-                                    }
-                                    resolve(molecule)
-                                }
-                            )
-                        } else {
-                            resolve(molecule)
+                        // An error occurred
+                        if (Err) {
+                            console.log(Err)
+                            process.exit()
+                            // reject(Err)
                         }
-                    }
 
-                }
-            )
+
+                        if (null === molecule) {
+                           // notFound(search)
+                            pkl.searchBySMILES(search.replace(/\(\)/g, ""), db, (molecule_from_pubchem) => {
+                                if (molecule_from_pubchem !== null) {
+                                    molecule_from_pubchem['json'] = MoleculeFactory(search)
+                                    molecule_from_pubchem['search'] = search
+                                    db.collection("molecules").insertOne(molecule_from_pubchem, (err, result) => {
+                                        console.log("Inserted molecule")
+                                        console.log(molecule_from_pubchem)
+                                        //process.exit()
+                                        if (err) {
+                                            console.log(err)
+                                            process.exit()
+                                        } else {
+                                            //onMoleculeAddedToDBCallback(search)
+                                            console.log('PKL molecule added to db')
+                                        }
+                                        resolve(molecule_from_pubchem)
+                                    })
+
+                                } else {
+                                    console.log("Molecule not foudn in pubchem")
+                                    console.log(search)
+                                    process.exit()
+                                }
+                            })
+                        } else {
+                            // molecule found in db
+                            // add json if required
+                            if (undefined === molecule.json) {
+                                const molecule_json = MoleculeFactory(search)
+                                db.collection('molecules').updateOne(
+                                    {CID: molecule.CID},
+                                    {$set: {json: molecule_json}},
+                                    {upsert: true},
+                                    (err, count, status) => {
+                                        if (err) {
+                                            console.log("Error adding json")
+                                            process.exit()
+                                        }
+                                        resolve(molecule)
+                                    }
+                                )
+                            } else {
+                                resolve(molecule)
+                            }
+                        }
+
+                    }
+                )
+
         }
-    )
+    ).catch(e=>{
+        console.log("Error:")
+        console.log(e)
+    })
+
+
 
 module.exports = MoleculeLookup
 
