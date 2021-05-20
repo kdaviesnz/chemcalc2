7f5b36438b3dc50a9cd4a8bb2e28feba1616c62e
Kevin <kdaviesnz@gmail.com>
2021-05-20 12:05:40 +1200

    ...

--- Commands/TransferProtonReverse.js
+++ Commands/TransferProtonReverse.js
@@ -2,14 +2,27 @@
 //const FindDoubleBondPair = require('./FindDoubleBondPair')
 const Reaction = require("../Components/State/Reaction")
 
-const TransferProtonReverse = (mmolecule, reagent, rule, DEBUG) => {
+const TransferProtonReverse =  (mmolecule, reagent, rule, DEBUG, horizontalCallback, horizontalFn, commands, i) => {
 
-    console.log("Calling TransferProtonReverse")
+    console.log("Calling TransferProtonReverse()")
+
+    const reaction = new Reaction([mmolecule,1], reagent, rule, DEBUG, horizontalCallback, horizontalFn, commands)
+    const result = reaction.reductiveAminationReverse()
+
+    if(undefined !== commands[i+1]) {
+        console.log("TransferProtonReverse -> next command")
+        console.log(commands[i+1])
+        horizontalCallback(i+1, horizontalCallback)
+    }
+
+    console.log("TransferProtonReverse -> result")
+    console.log(result)
     process.error()
-
-    const reaction = new Reaction(mmolecule, reagent, rule, DEBUG)
-
-    const result = reaction.transferProtonReverse()
+    // vertical callback
+    if (result !== false) {
+        const start = horizontalFn(result[0], result[1], commands)
+        start(0, horizontalCallback)
+    }
 
     return result  === false? false : [
         reaction.container_substrate,
